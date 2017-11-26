17ba8cd71d556ca220de4bef4a65dba1d36bd829
Grant Ammons <gammons@gmail.com>
2017-11-26 08:37:48 -0500

    A compton config that actually works well?

--- bin/compton.sh
+++ bin/compton.sh
@@ -4,4 +4,5 @@ killall -QUIT compton
 
 sleep 0.5
 
-compton -b --config $HOME/dotfiles-local/compton.conf
+compton -cCzG -t-3 -l-5 -r4 --config /dev/null --backend xrender --unredir-if-possible
+#compton -b --config $HOME/dotfiles-local/compton.conf
