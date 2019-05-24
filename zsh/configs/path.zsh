e83940b6a5823ea43cb86f3bbbdc4caeb0de9b63
Grant Ammons <gammons@gmail.com>
2019-05-24 06:44:18 -0400

    update path for android home

--- zsh/configs/path.zsh
+++ zsh/configs/path.zsh
@@ -4,6 +4,6 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
 export PATH="$PATH:$HOME/code/google-cloud-sdk/bin"
-export PATH="$PATH:/usr/local/opt/android-sdk/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_AVD_HOME"
