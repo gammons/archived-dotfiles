2803f262f4a1114377089ef449a4dfe46d8b579a
Grant Ammons <gammons@gmail.com>
2019-12-03 08:21:10 -0500

    remove a bunch of shit that i3-gnome replaces

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -2,23 +2,23 @@
 xrandr --dpi 92
 
 # copy over the right Xdefaults
-rm ~/.Xdefaults
-rm ~/.Xresources
-ln -s ~/.Xdefaults-big-monitor ~/.Xdefaults
-ln -s ~/.Xdefaults-big-monitor ~/.Xresources
+# rm ~/.Xdefaults
+# rm ~/.Xresources
+# ln -s ~/.Xdefaults-big-monitor ~/.Xdefaults
+# ln -s ~/.Xdefaults-big-monitor ~/.Xresources
 
 # reload xsessionrc
-~/.xsessionrc
+# ~/.xsessionrc
 
-xrandr --output DP-2 --auto
-xrandr --output eDP-1 --same-as DP-2
+# xrandr --output DP2 --auto
+# xrandr --output eDP-1 --same-as DP-2
 
 # update keyboard configuration
-setxkbmap -option ''
-setxkbmap -option ctrl:swapcaps -option altwin:hyper_win
+# setxkbmap -option ''
+# setxkbmap -option ctrl:swapcaps -option altwin:hyper_win
 
-rm ~/.config/alacritty/alacritty.yml
-ln -s ~/.config/alacritty/alacritty.yml.big-monitor ~/.config/alacritty/alacritty.yml
+# rm ~/.config/alacritty/alacritty.yml
+# ln -s ~/.config/alacritty/alacritty.yml.big-monitor ~/.config/alacritty/alacritty.yml
 
 # start imwheel to make scrolling faster
 killall imwheel # make idempotent
