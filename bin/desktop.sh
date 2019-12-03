564fdd9af2e5b1646a9728c0cb09d86fda5fdf0b
Grant Ammons <gammons@gmail.com>
2019-12-03 06:35:43 -0500

    update i3 to use i3-gnome stuff
    therefore I don't need a bunch of shit I normally need

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -1,25 +1,2 @@
-# set dpi
-xrandr --dpi 92
-
-# copy over the right Xdefaults
-rm ~/.Xdefaults
-rm ~/.Xresources
-ln -s ~/.Xdefaults-big-monitor ~/.Xdefaults
-ln -s ~/.Xdefaults-big-monitor ~/.Xresources
-
-# reload xsessionrc
-~/.xsessionrc
-
-xrandr --output DP-2 --auto
-xrandr --output eDP-1 --same-as DP-2
-
-# update keyboard configuration
-setxkbmap -option ''
-setxkbmap -option ctrl:swapcaps -option altwin:hyper_win
-
-rm ~/.config/alacritty/alacritty.yml
-ln -s ~/.config/alacritty/alacritty.yml.big-monitor ~/.config/alacritty/alacritty.yml
-
-# start imwheel to make scrolling faster
 killall imwheel # make idempotent
 imwheel
