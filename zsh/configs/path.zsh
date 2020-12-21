5de7f1e2dec1a0f2d36de81a23f5fdfe285d589b
Grant Ammons <gammons@gmail.com>
2020-12-21 08:45:47 -0500

    add .bin to path

--- zsh/configs/path.zsh
+++ zsh/configs/path.zsh
@@ -2,6 +2,7 @@ export GOPATH=$HOME/code/go
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 export PATH="$PATH:$HOME/bin"
+export PATH="$PATH:$HOME/.bin"
 export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
 export PATH="$PATH:$HOME/code/google-cloud-sdk/bin"
 export PATH="$PATH:$HOME/.local/bin"
