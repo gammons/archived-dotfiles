e2b03e9bcce4511430467617e1949c7766f88a24
Grant Ammons <gammons@gmail.com>
2021-05-29 09:05:31 -0400

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,13 +4,17 @@ if status --is-interactive
 end
 
 source ~/.aliases
+source ~/.asdf/asdf.fish
+
+source ~/.fzf/shell/key-bindings.fish
 
 set SHELL /usr/bin/fish
 
 if not set -q TMUX
-    set -g TMUX tmux new-session -d -s base
+    set DATE (date +%s)
+    set -g TMUX tmux new-session -d -s $DATE
     eval $TMUX
-    tmux attach-session -d -t base
+    tmux attach-session -d -t $DATE
 end
 
 starship init fish | source
