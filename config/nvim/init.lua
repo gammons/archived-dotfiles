17637dbed3862704b892a42b46c8173e8a7de344
Grant Ammons <gammons@gmail.com>
2021-09-14 06:25:04 -0400

    add this alias so I don't go insane

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -113,6 +113,7 @@ require'compe'.setup {
 
 cmd("autocmd FileType markdown call compe#setup({'enabled': v:false})")
 cmd("autocmd FileType gitcommit call compe#setup({'enabled': v:false})")
+cmd("command! Wq wq")
 
 -------------------- MAPPINGS ------------------------------
 
